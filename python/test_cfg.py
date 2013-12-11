8ecf28bc477965b6dd2c51534042d656edd0ccb7
Clint <clint.a.richardson@gmail.com>
2013-12-10 19:51:37 -0600

    made python cfg for mcTagger module

---
+++ python/test_cfg.py
@@ -0,0 +1,32 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Tagging")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32 ( -1 ) )
+
+#load in source file
+process.source = cms.Source( "PoolSource",
+                            fileNames = cms.untracked.vstring('file:./reco_DIGI_L1_DIGI2RAW.root'),
+                            inputCommands = cms.untracked.vstring('keep *'),
+                            )
+
+
+#run the producer
+process.Tagger = cms.EDProducer("MCTagger",
+                                genParticles = cms.InputTag("genParticles"),
+                                )
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string('histodemo.root')
+                                   )
+
+process.out = cms.OutputModule("PoolOutputModule",
+                                       fileName = cms.untracked.string('myOutputFile.root') ,
+                                       outputCommands = cms.untracked.vstring("keep *"),
+                                       )
+
+process.p = cms.Path(process.Tagger)
+
+process.e = cms.EndPath(process.out)
