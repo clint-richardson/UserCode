b37fa65a7488b465f84b43a148f6623e3831587f
Clint <clint.a.richardson@gmail.com>
2014-01-15 15:46:19 -0600

    adding new python files

---
+++ python/HadronTagger_cfi.py
@@ -0,0 +1,3 @@
+import FWCore.ParameterSet.Config as cms
+
+hadrontag = cms.EDFilter("HadronTagger")
--- python/__init__.py
+++ python/__init__.py
@@ -1,3 +1,3 @@
 #Automatically created by SCRAM
 import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/UserCode/CATopTagFilter/',1)[0])+'/cfipython/slc5_amd64_gcc472/UserCode/CATopTagFilter')
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/UserCode/TopTagMCMatcher/',1)[0])+'/cfipython/slc5_amd64_gcc462/UserCode/TopTagMCMatcher')
