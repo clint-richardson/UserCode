78eabe8e0ef123a1bc8371bbc7fca62cc9471730
Dylan <drankin@bu.edu>
2014-01-16 12:10:18 -0600

    pass histogram

--- plugins/MCTagger.cc
+++ plugins/MCTagger.cc
@@ -53,6 +53,7 @@ private:
   virtual void produce(edm::Event & event, const edm::EventSetup & EventSetup);
   virtual void EndJob(){};
   TH1F* hist;
+  TH1F* passhist;
 
 
   edm::InputTag             genParticles_it;
@@ -76,6 +77,7 @@ MCTagger::MCTagger(const edm::ParameterSet& Pset){
   //attempt to add histogram
   edm::Service<TFileService> fs;
   hist = fs->make<TH1F>("Tags", "tags", 4, 0 , 2);
+  passhist = fs->make<TH1F>("Passes", "passes", 3, 0 , 2);
 
 }
 void MCTagger::BeginJob(){ 
@@ -97,6 +99,7 @@ void MCTagger::produce(edm::Event& iEvent,const edm::EventSetup& iEventSetup){
 
   //First, check to see if the event has a boostedtop event, which is defined by the daughters having a deltaR<=0.8 (which is the size of our 
   //jet algorithm
+  bool topchk = false;
   int count = 0;
   for(size_t i = 0; i < GenColl->size();i++){
 
@@ -109,6 +112,7 @@ void MCTagger::produce(edm::Event& iEvent,const edm::EventSetup& iEventSetup){
       // int Mid = mother->pdgId();
       //make sure the decay is from a top (anti-top)
       if(abs(id)==6 ){
+	float PT = p.pt();
 	//now make sure the top decays only into W+/- and b(bbar)
 	if((abs( (p.daughter(0))->pdgId() + (p.daughter(1))->pdgId())==29)){
 	  //now I want to check the deltaR between the three daughter quarks, so first for check that the W decays hadronically
@@ -144,17 +148,20 @@ void MCTagger::produce(edm::Event& iEvent,const edm::EventSetup& iEventSetup){
 			 
 
 	  //double daughtDeltaR = mdeltaR( (p.daughter(0))->eta(), (p.daughter(0))->phi(), (p.daughter(1))->eta(),(p.daughter(1))->phi());
-	  if(maxdR<=0.8 && hadron){
+	  if(maxdR<=0.8 && hadron && PT >280.0){
 	    BTop->push_back(1);
-	    hist->Fill(1.0);
 	    for(unsigned int j=0;j<p.numberOfDaughters();j++){
 	      if(abs(p.daughter(j)->pdgId())==24){
 		if(abs(((p.daughter(j))->daughter(0))->pdgId())<11){
 		  count = count+1;
+                  if(!topchk) {
+                    topchk = true;
+                  }
+                  
 		}
 	      }
 	    }
-	    //cout<<"should get 1"<<endl;
+	    cout<<"max delta R = "<<maxdR<<" and top pt is "<<PT<<endl;
 	  }
 	  else{
 	    BTop->push_back(0);
@@ -175,6 +182,13 @@ void MCTagger::produce(edm::Event& iEvent,const edm::EventSetup& iEventSetup){
   iEvent.put( BTop, "BoostedTop");
 
   cout<<"Number of boosted jets is: "<<count<<endl;
+
+  if (topchk) {
+    passhist->Fill(1.0);
+  }
+  else {
+    passhist->Fill(0.0);
+  }
 
 }
 
